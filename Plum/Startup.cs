faf10269a74a4fdc7e478c2ed8175da62013fcdb
Kyle Jones <mynameiskyle242@gmail.com>
2020-08-23 14:55:45 -0400

    Fix issue with google authentication

--- Plum/Startup.cs
+++ Plum/Startup.cs
@@ -42,12 +42,15 @@ namespace Plum
                 options.ClientSecret = Configuration["Authentication:Google:ClientSecret"];
             });
 
+            // TODO - I commented this out because it was preventing me from being able to log in
+            /*
             services.Configure<CookiePolicyOptions>(options =>
             {
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
                 options.CheckConsentNeeded = context => true;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
+            */
 
             Dapper.DefaultTypeMap.MatchNamesWithUnderscores = true;
 
